be197054d3ddb65e5b26de5dc4f790ca2d09c379
Talukder-Asif <asif.talukder.aj.10@gmail.com>
2026-01-03 10:21:33 +0600

    Import Data from CSV and Json

---
+++ 03 Importing.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+#Read a CSV Data
+df = pd.read_csv("Pokemon.csv")
+print(df)
+
+# To show all data
+print(df.to_string())
+
+#Print data based on filtering
+print(df[df.Legendary == True].to_string())
